6b7d68211ee2ad8388f0207b02f6a67bb7e86611
victor <dvicxy@gmail.com>
2021-08-27 15:31:16 +0100

    4th commit

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -36,6 +37,134 @@ class _HomeScreenState extends State<HomeScreen> {
               _mapcontroller.complete(controller);
               _googleMapController = controller;
             },
+          ),
+          Positioned(
+            right: 0.0,
+            bottom: 0.0,
+            left: 0.0,
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.35,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20)),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black,
+                        blurRadius: 20,
+                        spreadRadius: 0.5,
+                        offset: Offset(0, -10)),
+                  ]),
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Hey!",
+                      style: GoogleFonts.sahitya(
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      "Where do you wanna go?",
+                      style: GoogleFonts.sahitya(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.05,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(5.0),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.black54,
+                                blurRadius: 6.0,
+                                spreadRadius: 0.5,
+                                offset: Offset(0.7, 0.7))
+                          ]),
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Icon(
+                              Icons.search,
+                              color: Colors.teal,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 5.0,
+                          ),
+                          Text(" Search"),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 24.0,
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.home),
+                        SizedBox(
+                          width: 12.0,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Add Home Address"),
+                            SizedBox(height: 4.0),
+                            Text(
+                              "Your home address",
+                              style: GoogleFonts.sahitya(
+                                  color: Colors.grey[600], fontSize: 12.0),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Divider(
+                      height: 2.0,
+                      color: Colors.black,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.work),
+                        SizedBox(
+                          width: 12.0,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Add Work Address"),
+                            SizedBox(height: 4.0),
+                            Text(
+                              "Your work address",
+                              style: GoogleFonts.sahitya(
+                                  color: Colors.grey[600], fontSize: 12.0),
+                            )
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
           )
         ],
       ),
